f01679e854c150a25e7918e17989e6a460f052be
iFew <chitpong@gmail.com>
2019-03-29 15:12:34 +0700

    Initial Project

---
+++ Controllers/BasicController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Aws4RequestSigner;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace aws_backend_post_websocket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicController : ControllerBase
+    {        
+
+        public IConfiguration _configuration { get; }
+        public BasicController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var endpointUri = _configuration["AWS:urlAPIGw"];
+
+            var signer = new AWS4RequestSigner(_configuration["AWS:Key"], _configuration["AWS:Secret"]);
+            var request = new HttpRequestMessage {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(endpointUri)
+            };
+
+            request = await signer.Sign(request, _configuration["AWS:Service"], _configuration["AWS:Region"]);
+
+            var client = new HttpClient();
+            var response = await client.SendAsync(request);
+
+            var responseStr = await response.Content.ReadAsStringAsync();
+
+            return responseStr;
+            
+        }
+    }
+}
---
+++ Controllers/SocketController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Aws4RequestSigner;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace aws_backend_post_websocket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocketController : ControllerBase
+    {        
+
+        public IConfiguration _configuration { get; }
+        public SocketController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<string> Post(InputModel input)
+        {
+            var endpointUri = _configuration["AWS:urlSocketAPI"] + input.connection_id;
+
+            var signer = new AWS4RequestSigner(_configuration["AWS:Key"], _configuration["AWS:Secret"]);
+            var request = new HttpRequestMessage {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(endpointUri),
+                Content = new StringContent(input.message)
+            };
+
+            request = await signer.Sign(request, _configuration["AWS:Service"], _configuration["AWS:Region"]);
+
+            var client = new HttpClient();
+            var response = await client.SendAsync(request);
+
+            var responseStr = await response.Content.ReadAsStringAsync();
+
+            return responseStr;
+            
+        }
+    }
+
+    public class InputModel
+    {
+        public string connection_id { get; set;}
+        public string message { get; set;}
+    }
+}
